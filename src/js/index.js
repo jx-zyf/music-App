42748da61cc414f342d6af559f4f3aec11d7ae2e
jx-zyf <linxunzyf@gmail.com>
2017-09-16 17:00:35 +0800

    增加搜索

--- src/js/index.js
+++ src/js/index.js
@@ -37,11 +37,11 @@
                             <p>${item.singer}<span>${item.description}</span></p>
                         </div>
                     </a>`);
-                    $('.musicList').append($a);
+                    $('.hot_music>.musicList').append($a);
                 });
                 // 请求过的标志
                 $('.content>ul>li').eq(index).attr('data-downloaded','yes');
-                $('.musicList').append($fullList);
+                $('.hot_music>.musicList').append($fullList);
                 $('.loading').remove();
             },function(err){
                 console.log('error:'+err);
@@ -51,25 +51,99 @@
 })();
 
 // 搜索页
+// 搜索框逻辑
 (function focusShowCancel(){
-    $('.search_input input').on('focus',function(){
+    $('#search').on('focus',function(){
+        // 获得焦点时取消按钮出现，热门搜索隐藏
         $('.search_cancel').css('display','block').animate({
             opacity:1
         },600);
         $('.hot_search').css('display','none');
     }).on('blur',function(){
+        // 失去焦点时取消按钮隐藏
         $('.search_cancel').animate({
             opacity:0
         },600);
-        $('.search_input input').val('');
         setTimeout(function(){
             $('.search_cancel').css('display','none');
         },600)
-        $('.hot_search').css('display','block');
+        // 失去焦点 并value为空 热门搜索出现
+        if($('#search').val()===''){
+            $('.hot_search').css('display','block');
+        }
     });
+
     // $('.search_cancel').on('click',function(){
     //     $('.search_cancel').animate({
     //         opacity:0
     //     },600);
     // });
 })();
+// 搜索
+async function search(keyword){
+    var searchResult=[];
+    var database;
+    // 异步操作
+    if(keyword=='') return;
+    await $.get('./music.json').then(function(result){
+        database=result;
+        var search_result=database.filter(function(item){
+            return item.name.indexOf(keyword)>=0
+                    ||item.name.indexOf(keyword.toLowerCase())>=0
+                    ||item.name.indexOf(keyword.toUpperCase())>=0
+                    ||item.singer.indexOf(keyword)>=0
+                    ||item.singer.indexOf(keyword.toLowerCase())>=0
+                    ||item.singer.indexOf(keyword.toUpperCase())>=0
+                    ||item.description.indexOf(keyword)>=0
+                    ||item.description.indexOf(keyword.toLowerCase())>=0
+                    ||item.description.indexOf(keyword.toUpperCase())>=0;
+        })
+        searchResult=search_result.slice(0);
+    },function(err){
+        console.log('error:'+err);
+    });
+    // 等异步操作完成后再返回
+    return searchResult;
+}
+// search('云长').then(function(res){
+//     console.log(res);
+// });
+// 显示搜索结果
+function showList(){
+    var keyword=$('#search').val();
+    search(keyword).then(function(res){
+        // 没有输入关键字
+        if(res==undefined){
+            return;
+        }
+        // 没找到结果
+        if(res.length===0){
+            $('.search .musicList').append('<div class="no_result">暂无结果</div>');
+        }else{
+            res.forEach(function(item){
+                let $li=$(`<a href="javascript:;">
+                    <div class="musicContent">
+                        <h3>${item.name}</h3>
+                        <p>${item.singer}<span>${item.description}</span></p>
+                    </div>
+                </a>`);
+                $('.search .musicList').append($li);
+            })
+        }
+    })
+    // oninput/onpropertchange value变化就触发，
+    // 而onchange触发事件必须满足两个条件：
+    // a）当前对象属性改变，并且是由键盘或鼠标事件激发的（脚本触发无效）
+    // b）当前对象失去焦点(onblur)
+    $('#search').on('change input propertychange',function(){
+        if($('#search').val()==''){
+            // 清空搜索结果
+            $('.search .musicList').empty();
+        }
+    });
+}
+document.addEventListener('keyup',function(e){
+    if(e.keyCode===13){
+        showList();
+    }
+})
