68cbfda02fe3bf53d518e59027729133ff98deb0
jx-zyf <linxunzyf@gmail.com>
2017-09-26 21:10:56 +0800

    进度条点击调进度

--- src/play/play.js
+++ src/play/play.js
@@ -27,16 +27,19 @@ function setInfo(bgimg,coverimg,name,singer){
     $('.song_info>span').text(singer);
 }
 
+// 歌曲播放
 function play(url){
-    var $audio=$('.audio');
+    let $audio=$('.audio');
     let mPlay=$('.icon_play');
     let endminute,endsecond,endTime;
+    let timer=null;
+    let curTime;
     $audio.attr('src',url);
     $audio.on('canplay',function(){
         $audio[0].play();
         // 计算歌曲总时长
-        let endminute=Math.floor(this.duration/60);
-        let endsecond=Math.floor(this.duration%60);
+        endminute=Math.floor(this.duration/60);
+        endsecond=Math.floor(this.duration%60);
         endTime=`${doublee(endminute)}:${doublee(endsecond)}`;
         $('.time_end').text(endTime);
     });
@@ -56,9 +59,64 @@ function play(url){
             $audio[0].play();
             mPlay.css('background-position','center 69.5%')
             mPlay.attr('data-play','true');
+            run();
         }
     })
-    setInterval(function(){
+    // 歌词与进度条同步
+    run();
+    // 收藏
+    $('.m_like').on('click',function(){
+        if($(this).attr('data-like')==='true'){
+            $(this).css('background-position','left 9%');
+            $(this).attr('data-like','false');
+        }else{
+            $(this).css('background-position','left top');
+            $(this).attr('data-like','true');
+        }
+    })
+    // 点击进度条调进度
+    $('.progress_play').on('click',function(e){
+        // 获取点击位置相对于整个进度条的比例
+        let pro_play=e.offsetX/this.offsetWidth;
+        // 歌曲总时长 以秒为单位
+        let fullTime=endminute*60+endsecond;
+        // 目标时长
+        let targetTime=Math.round(fullTime*pro_play);
+        mPlay.css('background-position','center 69.5%')
+        mPlay.attr('data-play','true');
+        // 当前播放时间为点击的目标时间
+        $audio[0].currentTime=targetTime;
+        curTime=`${doublee(Math.floor(targetTime/60))}:${doublee(targetTime%60)}`;
+		$('.time_now').text(curTime);
+        $('.now_progress').css('width',pro_play*$('.progress_play').width());
+        run();
+    })
+}
+
+function getlyric(lyric){
+    let arr=lyric.split('\n');
+    let reg=/^\[(.+)\](.*)$/;
+    arr=arr.map(function(item){
+        let lyricInfo=reg.exec(item);
+        if(lyricInfo){
+            return {
+                time:lyricInfo[1],
+                words:lyricInfo[2]
+            }
+        }
+    });
+    arr.forEach(function (item) {
+        let $p =$('<p></p>') 
+        if (typeof item === 'object' && item.hasOwnProperty('words')) {
+            $p.attr('data-time',item.time).text(item.words)
+        }
+        $p.appendTo($('.lyric'))
+    });
+}
+
+function run(){
+    let $audio=$('.audio');
+    timer=setInterval(function(){
         //计算当前播放时间
 		let curminute=Math.floor($audio[0].currentTime/60);
 		let cursecond=Math.floor($audio[0].currentTime%60);
@@ -82,37 +140,6 @@ function play(url){
             $('.lyric').css('transform',`translateY(-${speed}px)`);
         }
     },500)
-    // 收藏
-    $('.m_like').on('click',function(){
-        if($(this).attr('data-like')==='true'){
-            $(this).css('background-position','left 9%');
-            $(this).attr('data-like','false');
-        }else{
-            $(this).css('background-position','left top');
-            $(this).attr('data-like','true');
-        }
-    })
-}
-
-function getlyric(lyric){
-    let arr=lyric.split('\n');
-    let reg=/^\[(.+)\](.*)$/;
-    arr=arr.map(function(item){
-        let lyricInfo=reg.exec(item);
-        if(lyricInfo){
-            return {
-                time:lyricInfo[1],
-                words:lyricInfo[2]
-            }
-        }
-    });
-    arr.forEach(function (item) {
-        let $p =$('<p></p>') 
-        if (typeof item === 'object' && item.hasOwnProperty('words')) {
-            $p.attr('data-time',item.time).text(item.words)
-        }
-        $p.appendTo($('.lyric'))
-    });
 }
 
 //格式化时间
